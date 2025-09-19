e7a3037d26c973990bbda1a3e675ad3b622fc306
JohannaB <johanna_b_g@hotmail.com>
2025-09-19 11:31:29 -0500

    refactor(frontend): se actualizan rutas de navegación

--- SENA/src/views/InstructorDashboard.vue
+++ SENA/src/views/InstructorDashboard.vue
@@ -61,12 +61,12 @@ const router = useRouter()
 
 //Redireccionar al formato grupal
 function irAGrupal() {
-  router.back()
+  router.push('/groupformat')
 }
 
 //Redireccionar al formato individual
 function irAIndividual() {
-  router.back()
+  router.push('/individualformat')
 }
 
 </script>
