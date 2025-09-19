43e0855ae59ba8a8f7a89dbcc3d1fb27987ae171
JohannaB <johanna_b_g@hotmail.com>
2025-09-19 11:29:56 -0500

    feat(frontend): se añade variable para verificar la modalidad

--- SENA/src/components/EditAprendizModal.vue
+++ SENA/src/components/EditAprendizModal.vue
@@ -77,6 +77,7 @@ async function envioDatosAprendiz(aprendiz) {
 const props = defineProps<{
     aprendiz: Aprendiz | null;
     mostrar: boolean;
+    esIndividual: boolean;
 }>()
 
 const emit = defineEmits<{
