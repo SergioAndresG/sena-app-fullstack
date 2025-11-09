7467b0451c53c013b77f83a0265dccb098f57788
SergioAndresG <140452759+SergioAndresG@users.noreply.github.com>
2025-11-08 21:42:19 -0500

    documentation(main.py): Se agregan comentarios

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# imporatciones basicas
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
@@ -8,44 +9,20 @@ from ENDPOINTS.aprendices import router_aprendices
 from ENDPOINTS.login import router_login
 from ENDPOINTS.usuarios import router_usuarios
 
-
+# Funcionese nesarias para las tareas
 from FUNCIONES.FUNCIONES_FICHAS.delete_fichas import clean_old_db_tokens, cleanup_old_db_records
 from FUNCIONES.FUNCIONES_TOKENS.tokens_service import cleanup_expired_tokens
-from MODELS.a_usuarios import Usuarios
-from MODELS.archivo_excel import ArchivoExcel
+from FUNCIONES.FUNCIONES_FICHAS.delete_reports import cleanup_old_reports
 
-
+# Midelware implementado
 from MIDELWARE.security_middleware import SecurityMiddleware
 
-# --- Imports para el Scheduler ---
-import os
-import datetime
+# Linreria para los jobs en segundo plano
 from apscheduler.schedulers.background import BackgroundScheduler
 
 app = FastAPI(title="SENA - Procesador de Fichas")
 
-# --- Lógica del Scheduler para Limpieza de Archivos ---
-REPORTS_DIR = "archivos_exportados"
 scheduler = BackgroundScheduler(daemon=True)
-
-def cleanup_old_reports():
-    """Elimina los archivos de reporte con más de 30 días de antigüedad."""
-    now = datetime.datetime.now()
-    cutoff = now - datetime.timedelta(days=30)
-    
-    if not os.path.exists(REPORTS_DIR):
-        return
-
-    for root, dirs, files in os.walk(REPORTS_DIR):
-        for file in files:
-            file_path = os.path.join(root, file)
-            try:
-                file_modified_time = datetime.datetime.fromtimestamp(os.path.getmtime(file_path))
-                if file_modified_time < cutoff:
-                    os.remove(file_path)
-                    print(f"Eliminado archivo antiguo: {file_path}")
-            except Exception as e:
-                print(f"Error eliminando {file_path}: {e}")
 
 @app.on_event("startup")
 def start_scheduler():
